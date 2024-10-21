c15e23d03f50d2b7fa5cdb0254cde46dcfa2ab5d
salisalinas <sali.alvaro@gmail.com>
2024-10-21 16:25:52 +0200

    Some fixes

--- todo-list-angular/src/app/components/todo-form/todo-form.component.ts
+++ todo-list-angular/src/app/components/todo-form/todo-form.component.ts
@@ -1,6 +1,8 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TodoService } from '../../services/todo.service';
+import { Observable } from 'rxjs';
+import TodoItem from '../../interfaces/todoItem.interface';
 
 @Component({
   selector: 'app-todo-form',
@@ -10,22 +12,22 @@ import { TodoService } from '../../services/todo.service';
   styleUrl: './todo-form.component.css',
 })
 export class TodoFormComponent {
-  @Output() newTaskAdded = new EventEmitter<string>();
-  newTask: string = '';
-  newDescription: string = '';
+  todoItem$: Observable<TodoItem> = new Observable<TodoItem>();
+  formName: string = '';
+  
+  formDescription: string = '';
 
   constructor(private todoService: TodoService) {}
   async addTask(): Promise<void> {
-    if (this.newTask.trim() !== '') {
-      this.newTaskAdded.emit(this.newTask);
+    if (this.formName.trim() !== '') {
       await this.todoService.addItem({
         id: Date.now().toString(),
-        name: this.newTask,
-        description: this.newDescription,
+        name: this.formName,
+        description: this.formDescription,
         completed: false,
       });
-      this.newTask = '';
-      this.newDescription = '';
+      this.formName = '';
+      this.formDescription = '';
     }
   }
 }
--- todo-list-angular/src/app/services/todo.service.ts
+++ todo-list-angular/src/app/services/todo.service.ts
@@ -10,26 +10,26 @@ import TodoItem from '../interfaces/todoItem.interface';
 })
 export class TodoService {
   private firestore: Firestore = inject(Firestore);
-
+  //Obtener todas las tareas
   getItems(): Observable<TodoItem[]> {
     const itemCollection = collection(this.firestore, 'items');
     return collectionData(itemCollection, { idField: 'id' }) as Observable<TodoItem[]>;
   }
-
+  //Obtener una tarea
   getItem(id: string): Observable<TodoItem> {
     const itemDoc = doc(this.firestore, `items/${id}`);
     return docData(itemDoc, { idField: 'id' }) as Observable<TodoItem>;
   }
-
+  //Añadir tarea 
   async addItem(item: TodoItem) {
     await addDoc(collection(this.firestore, 'items'), item);
   }
-
+  //Actualizar tarea
   async updateItem(id: string, item: Partial<TodoItem>) {
     const itemDoc = doc(this.firestore, `items/${id}`);
     await updateDoc(itemDoc, item);
   }
-
+  //Borrar tarea
   async deleteItem(id: string) {
     const itemDoc = doc(this.firestore, `items/${id}`);
     await deleteDoc(itemDoc);
